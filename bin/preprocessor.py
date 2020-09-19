9c47006034c537e78e62e83e5731d5024311d4d9
Isha Gulati <isha.gulati@live.com>
2020-09-19 16:35:32 +0530

    completed code for review

--- bin/preprocessor.py
+++ bin/preprocessor.py
@@ -1,14 +1,10 @@
 import re
-import yaml
 
 class Preprocessor_doc:
     """ 
     Module for preprocessing
     """
-    def __init__(self):
-        self.x = 2
-
-    def remove_special_char(self):
+    def remove_special_char(self,text):
         """ 
         Remove special characters
         Input 
@@ -16,24 +12,10 @@ class Preprocessor_doc:
         Output 
             modified_text : string
         """
-        pass
+        modified_text = re.sub(',|;|#,$','',text)
+        return modified_text
 
-    def tokenize_article(self):
-        """
-        Split sentences into tokens
-        Input
-            modified_text : string
-        Output 
-            tokenized_text : string
-        """
-        pass
+    def convert_to_lower(self,text):
+        return text.lower()
 
-    def stopowords_removal(self):
-        """
-        Remove stopwords 
-        Input 
-            tokenized_text : string
-        Output
-            cleaned_text : string
-        """
-        pass
+        
--- bin/summarizer.py
+++ bin/summarizer.py
@@ -1,16 +1,22 @@
 import yaml 
+from preprocessor import Preprocessor_doc
+
 class Summarizer_doc:
     """
     Module for summarization
 
     """
     def __init__(self):
-        with open('../config/config.yaml','r') as fl:
-            self.configs = yaml.load(fl) 
+        with open('../config/config.yaml','r',encoding='utf-8') as fl:
+            self.config = yaml.load(fl) 
 
     def load_docs(self,file_path):
         """
         Read articles from path
+        Input
+            file_path : path of file
+        Output 
+            text : contents of file
         """
         with open(file_path,'r') as fl:
             text = fl.read()
@@ -47,17 +53,24 @@ class Summarizer_doc:
         top3_idx = sortedIdx[-3:]
         top_3_sentences = [sentences[i] for i in top3_idx]
         return top_3_sentences
+
+    def preprocess(self,text):
+        preprocess_obj = Preprocessor_doc()
+        modified_text = preprocess_obj.remove_special_char(text)
+        lower_text = preprocess_obj.convert_to_lower(modified_text)
+        return lower_text
     
     def find_summary(self):
         file_path = self.config['data_path']['train_data']
         text = self.load_docs(file_path)
-        sentences = self.sent_splitter(text)
+        filtered_text = self.preprocess(text)
+        sentences = self.sent_splitter(filtered_text)
         first_sent,rest_sent = self.group_sentences(sentences)
         sent_lengths = self.find_sent_length_array(rest_sent)
         top_3_sentences = self.find_top_sentence(sent_lengths,rest_sent)
         all_sentences = [first_sent] + [top_3_sentences]
         summary = ''.join(all_sentences)
-        
+
 
 summarize_obj = Summarizer_doc()
-summarize_obj.load_config()
+summarize_obj.find_summary()
